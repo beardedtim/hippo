6a9701db0b3cbd1096112459806e0f1e813c5847
Tim Roberts <timroberts@fastmail.org>
2020-06-25 08:24:45 -0400

    Add Middleware and Error Handling
    This commit adds the ability to have middleware in our functions
    along with some middleware for error handling.
    
    We also also add some classes so we can throw errors and they
    all look the same

--- src/start.js
+++ src/start.js
@@ -1,5 +1,9 @@
-const createLog = require('@/log')
 const path = require('path')
+
+const createLog = require('@/log')
+const Server = require('@/server')
+const middleware = require('@/middleware')
+const httpErrors = require('@/errors/http')
 
 const log = createLog({
   /**
@@ -23,16 +27,16 @@ const log = createLog({
    * Where we send the logs. If this value is undefined, it will
    * fallback to transports.console
    */
-  transports: [
-    /**
-     * The File Transport appends the logs to `/<path>/<method>.log` file
-     */
-    createLog.transports.file(path.resolve(__dirname, '..', 'logs')),
-    /**
-     * The Console Transport logs to stdout
-     */
-    createLog.transports.console
-  ],
+  // transports: [
+  //   /**
+  //    * The File Transport appends the logs to `/<path>/<method>.log` file
+  //    */
+  //   createLog.transports.file(path.resolve(__dirname, '..', 'logs')),
+  //   /**
+  //    * The Console Transport logs to stdout
+  //    */
+  //   createLog.transports.console
+  // ],
   /**
    * Each time we create a message to log, we get a chance to serialize the
    * object. In this way, if someone gives us an Express `request` object, we
@@ -46,6 +50,40 @@ const log = createLog({
       }
     }
 
+    if (message.req) {
+      message.req = {
+        url: message.req.url,
+        method: message.req.method,
+        headers: message.req.headers
+      }
+    }
+
     return message
   }
 })
+
+const server = new Server({ log })
+
+process.on('uncaughtException', err => {
+  server.stop(() => {
+    log.fatal({ err }, 'UNCAUGHT EXCEPTION')
+    process.exit(1)
+  })
+})
+
+process.on('unhandledRejection', err => {
+  server.stop(() => {
+    log.fatal({ err }, 'UNHANDLED REJECTION')
+    process.exit(1)
+  })
+})
+
+server
+  .start(5001, () => log.info('Server started'))
+  .use(middleware.request_id())
+  .use(middleware.catch_errors())
+  .use(middleware.log_errors())
+  .use(middleware.log_request())
+  .use(async (ctx, next) => {
+    throw new httpErrors.TeaPot()
+  })
